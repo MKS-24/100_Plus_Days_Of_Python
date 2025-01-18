eccef23beb5f4beaad9b01add802ccb3e7d724e9
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-01-17 23:19:03 -0800

    Add files via upload

---
+++ 23_name==main/52_name_is_main.py
@@ -0,0 +1,4 @@
+import ApnaModule_51
+
+# ager me module file se if  __name == "__main__" hatadu tu jo waha print howa wa hai wo bhi print hojae ga aevi
+ApnaModule_51.kaamKiya(1)
---
+++ 23_name==main/ApnaModule_51.py
@@ -0,0 +1,10 @@
+def kaamKiya(a):
+    print("Ha Kerliya Kaam") if a==1 else print("Nh Kiya Kaam")
+
+def habhai():
+    print("Ha Kerliya Kaam")
+
+if __name__ == "__main__":
+    poochu = "Yess"
+    kaamKiya(0)
+    kaamKiya(1)
